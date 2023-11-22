def74ff5c56fd42cf629f27cf541c8cdf80043bc
Vijay Singh <vijaysinghaashu@gmail.com>
2023-11-22 18:39:13 +0530

    Use model mapper and model struct library and add global exception

---
+++ src/main/java/com/excellence/springboot_restful_webservices/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.excellence.springboot_restful_webservices.mapper;
+
+import com.excellence.springboot_restful_webservices.dto.UserDto;
+import com.excellence.springboot_restful_webservices.entity.User;
+
+public class UserMapper {
+
+	// Convert User JPA Entity into UserDto
+	public static UserDto mapToUserDto(User user) {
+		UserDto userDto = new UserDto(user.getId(), user.getFirstName(), user.getLastName(), user.getEmail());
+		return userDto;
+	}
+
+	// Convert UserDto into User JPA Entity
+	public static User mapToUser(UserDto userDto) {
+		User user = new User(userDto.getId(), userDto.getFirstName(), userDto.getLastName(), userDto.getEmail());
+		return user;
+	}
+}
